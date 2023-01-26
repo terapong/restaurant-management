7d5f3d6efcb25675391f943d689cac7b0677df42
terapong <terapong.p@gmail.com>
2023-01-26 15:16:53 +0700

    toto new restaurant

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"os"
+	"github.com/gin-gonic/gin"
+	"golang-restaurant-management/database"
+	"golang-restaurant-management/routes"
+	"golang-restaurant-management/middleware"
+	"go.mongodb.org/mongo-driver/mongo"
+)mongo.Collection = database.OpenCollection(database.Client, "food")
+
+func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8000"
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+	routes.UserRoutes(router)
+	router.Use(middleware.Authentication())
+
+	routes.FoodRoutes(router)
+	routes.MenuRoutes(router)
+	routes.TableRoutes(router)
+	routes.OrderRoutes(router)
+	routes.OrderItemRoutes(router)
+
+	router.Run(":" + port)
+}
