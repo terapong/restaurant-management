50912345759b864f2611b8618d5fb3a02d09ea05
terapong <terapong.p@gmail.com>
2023-01-31 16:06:26 +0700

    toto 31_1_2023 Controllers OK 1

--- controllers/menuController.go
+++ controllers/menuController.go
@@ -1,10 +1,34 @@
 package controller
 
-import "gitnub.com/gin-gonic/gin"
+import (
+	"context"
+	"fmt"
+	"golang-restaurant-management/database"
+	"golang-restaurant-management/models"
+	"net/http"
+	"time"
+	"log"
+	"gitnub.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
+	"gopkg.in/bluesuncorp/validator.v5"
+	"gopkg.in/mgo.v2/bson"
+)
+
+var menuCollection *mongo.Collection = database.OpenCollection(database.Client, "menu")
 
 func GetMenus() gin.HandlerFunc {
 	return func(c *gin.Context) {
-
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		result, err := menuCollection.Find(context.TODO(), bson.M{})
+		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occured while listing the menu items"})
+		} 
+		var allMenus []bson.M
+		if err = result.All(ctx, &allMenus); err != nil {
+			log.Fatal(err)
+		}
+		c.JSON(http.SttusOK, allMenus)
 	}
 }
 
